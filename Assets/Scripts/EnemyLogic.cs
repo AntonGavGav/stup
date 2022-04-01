b90d742f02b8edaf5e58daca067052d82897e592
EventfulLynx915 <gavrylenko.anton@gmail.com>
2022-04-01 13:41:15 +0200

    Add pigeon name and add pigeon atack animation

--- Assets/Scripts/EnemyLogic.cs
+++ Assets/Scripts/EnemyLogic.cs
@@ -3,20 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 public class EnemyLogic : MonoBehaviour
 {
     private int enemyHealth = 100;
+    private float timeForColorChanging = 0.15f;
 
     public HealthBar healthBar;
 
     [SerializeField] private Transform playerTransform;
     [SerializeField] private Animator animator;
+    [FormerlySerializedAs("spriteRender")] [SerializeField] private Material material;
 
     private NavMeshAgent agent;
-
+    private Color defaultColor;
+    
     private void Start()
     {
+        defaultColor = material.color;
+        
         healthBar.SetMaxHealth(enemyHealth);
         agent = transform.GetComponent<NavMeshAgent>();
     }
@@ -24,8 +30,9 @@ public class EnemyLogic : MonoBehaviour
     private void Update()
     {
         animator.SetFloat("Blend", agent.velocity.magnitude);
-        if (Vector3.Distance(transform.position, playerTransform.position) < 5.5f)
+        if (Vector3.Distance(transform.position, playerTransform.position) < 2f)
         {
+            animator.SetBool("Atack", true);
             agent.isStopped = true;
             Vector3 point = new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z);
             SmoothlyRotateTowardsObj(playerTransform, 0.01f);
@@ -33,6 +40,7 @@ public class EnemyLogic : MonoBehaviour
         }
         else
         {
+            animator.SetBool("Atack", false);
             agent.isStopped = false;
             agent.SetDestination(playerTransform.position); 
         }
@@ -46,11 +54,22 @@ public class EnemyLogic : MonoBehaviour
     }
     public void ApplyDamage(int damage)
     {
+        StartCoroutine("SwitchColors");
         enemyHealth -= damage;
         healthBar.SetHealth(enemyHealth);
         if (enemyHealth <= 0)
         {
+            material.color = new Color(1f, 0.30196078f, 0.30196078f);
             Destroy(gameObject);
+            material.color = defaultColor;
         }
+    }
+
+    IEnumerator SwitchColors()
+    {
+        yield return new WaitForSeconds(0.05f);
+        material.color = new Color(1f, 0.30196078f, 0.30196078f);
+        yield return new WaitForSeconds(timeForColorChanging);
+        material.color = defaultColor;
     }
 }
