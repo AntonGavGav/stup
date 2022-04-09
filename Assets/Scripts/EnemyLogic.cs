0da2b9b562b8924d20f986bf0d0e22b9476ab27b
EventfulLynx915 <gavrylenko.anton@gmail.com>
2022-04-09 20:14:00 +0200

    Add ragdoll on death

--- Assets/Scripts/EnemyLogic.cs
+++ Assets/Scripts/EnemyLogic.cs
@@ -14,38 +14,44 @@ public class EnemyLogic : MonoBehaviour
     public HealthBar healthBar;
 
     [SerializeField] private Transform playerTransform;
-    [SerializeField] private Animator animator;
     [SerializeField] private GameObject damageText;
+    [SerializeField] private Rigidbody torso;
     [FormerlySerializedAs("material")] [FormerlySerializedAs("spriteRender")] [SerializeField] private Material Defualtmaterial;
     [FormerlySerializedAs("material")] [FormerlySerializedAs("spriteRender")] [SerializeField] private Material Redmaterial;
-
+    
+    private Animator animator;
     private NavMeshAgent agent;
     private Color defaultColor;
+    private bool isDead = false;
     
     private void Start()
     {
+        animator = transform.GetChild(0).GetComponent<Animator>();
         defaultColor = Defualtmaterial.color;
-        
         healthBar.SetMaxHealth(enemyHealth);
         agent = transform.GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
-        animator.SetFloat("Blend", agent.velocity.magnitude);
-        if (Vector3.Distance(transform.position, playerTransform.position) < 2f)
+        if (!isDead)
         {
-            animator.SetBool("Atack", true);
-            agent.isStopped = true;
-            Vector3 point = new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z);
-            SmoothlyRotateTowardsObj(playerTransform, 0.01f);
-            //transform.LookAt(point);
-        }
-        else
-        {
-            animator.SetBool("Atack", false);
-            agent.isStopped = false;
-            agent.SetDestination(playerTransform.position); 
+            animator.SetFloat("Blend", agent.velocity.magnitude);
+            if (Vector3.Distance(transform.position, playerTransform.position) < 2f)
+            {
+                animator.SetBool("Atack", true);
+                agent.isStopped = true;
+                Vector3 point = new Vector3(playerTransform.position.x, transform.position.y,
+                    playerTransform.position.z);
+                SmoothlyRotateTowardsObj(playerTransform, 0.01f);
+                //transform.LookAt(point);
+            }
+            else
+            {
+                animator.SetBool("Atack", false);
+                agent.isStopped = false;
+                agent.SetDestination(playerTransform.position);
+            }
         }
     }
 
@@ -57,16 +63,20 @@ public class EnemyLogic : MonoBehaviour
     }
     public void ApplyDamage(int damage)
     {
-        GameObject damageText1 =  Instantiate(damageText, transform.position, Quaternion.identity);
-        damageText1.transform.GetComponent<TextMeshPro>().text = damage.ToString();
-        StartCoroutine("SwitchColors");
-        enemyHealth -= damage;
-        healthBar.SetHealth(enemyHealth);
-        if (enemyHealth <= 0)
+        if (!isDead)
         {
-            transform.GetChild(0).GetChild(0).GetComponent<Renderer>().sharedMaterial = Redmaterial;
-            Destroy(gameObject);
-            transform.GetChild(0).GetChild(0).GetComponent<Renderer>().sharedMaterial = Defualtmaterial;
+            GameObject damageText1 = Instantiate(damageText, transform.position, Quaternion.identity);
+            damageText1.transform.GetComponent<TextMeshPro>().text = damage.ToString();
+            StartCoroutine("SwitchColors");
+            enemyHealth -= damage;
+            healthBar.SetHealth(enemyHealth);
+            if (enemyHealth <= 0)
+            {
+                Death();
+                transform.GetChild(0).GetChild(0).GetComponent<Renderer>().sharedMaterial = Redmaterial;
+                //Destroy(gameObject);
+                transform.GetChild(0).GetChild(0).GetComponent<Renderer>().sharedMaterial = Defualtmaterial;
+            }
         }
     }
 
@@ -76,5 +86,24 @@ public class EnemyLogic : MonoBehaviour
         transform.GetChild(0).GetChild(0).GetComponent<Renderer>().sharedMaterial = Redmaterial;
         yield return new WaitForSeconds(timeForColorChanging);
         transform.GetChild(0).GetChild(0).GetComponent<Renderer>().sharedMaterial = Defualtmaterial;
+    }
+
+    void Death()
+    {
+        isDead = true;
+        animator.enabled = false;
+        agent.enabled = false;
+        transform.GetComponent<CapsuleCollider>().enabled = false;
+        healthBar.HealthBarDestroy();
+        Collider[] colliders = gameObject.GetComponentsInChildren<Collider>();
+        Rigidbody[] rigidbodies = gameObject.GetComponentsInChildren<Rigidbody>();
+        foreach(Collider collider in colliders){
+            collider.enabled = true;
+        }
+        foreach (Rigidbody rb in rigidbodies)
+        {
+            rb.isKinematic = false;
+        }
+        torso.AddForce(transform.forward*10, ForceMode.Impulse);
     }
 }
